e6217fb8d6b5d09872f43a05f5191431620c0ce1
toxakaz <anton.a.kazancev@gmail.com>
2021-11-18 13:40:33 +0700

    AST in module

--- AST.ml
+++ AST.ml
@@ -1,29 +1,25 @@
-type num =
-| Int of int
-| Long of int
-| Float of float
-| Double of float
+module AST = struct
+  type num =
+  | Int of int
+  | Long of int
+  | Float of float
+  | Double of float
 
-type value = 
-| Num of num
-| String of string
+  type value = 
+  | Num of num
+  | String of string
 
-type var =
+  type expr =
+  | Val of value
+  | Var of string
+  | Sum of expr * expr
+  | Sub of expr * expr
+  | Mul of expr * expr
+  | Div of expr * expr
+  | Func of func
+  and func =
   {
     name : string;
-    value : value;
+    args : expr list;
   }
-
-type expr =
-| Val of value
-| Var of var
-| Sum of expr * expr
-| Sub of expr * expr
-| Mul of expr * expr
-| Div of expr * expr
-| Func of func
-and func =
-{
-  name : string;
-  args : expr list;
-}
+end
